87c9cc72c3a7ba21d3b7d668c343df3a6295bd86
mcra0009 <mcra0009@student.monash.edu>
2024-04-07 21:14:40 +1000

    added error handling and logs

--- src/handler.go
+++ src/handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 	"net/http"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
@@ -26,25 +27,41 @@ func RequestHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+	log.Println("✅ Config yaml retrieved")
 
 	applicationData, err := readApplicationData(applicationPath, config)
 	if err != nil {
 		panic(err)
 	}
+	log.Println("✅ application data retrieved")
 
 	//establish PubSub client and topic
 	pubSubClient, err := EstablishpubSubClient(ctx, applicationData.Project, userAgent)
 	if err != nil {
 		panic(err)
 	}
+	log.Println("✅ pubsub client retrieved")
 
 	pubSubTopic := EstablishPubSubTopic(ctx, pubSubClient, pubSubTopic)
 
 	//establish GSMSecret client
 	gsmClient, err := EstalisbGSMSecretClient(ctx, applicationData.Project)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("✅ gsm client retrieved")
 
 	encryptionToken, err := RetrieveGSMSecret(ctx, gsmClient, applicationData.GsmSecretRef)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("✅ gsm secret retrieved")
 
 	payloadIsValid := ValidatePayload(r.Header, encryptionToken)
 
+	if payloadIsValid {
+		//Send payload to pubsub
+	} else {
+		log.Println("Unable to verify payload sent with path", r.URL.Path)
+	}
 }
